794891f3d5c7fd33b27293d3e283a9c06000499d
Benjamin Santos <67348724+programmableGun@users.noreply.github.com>
2022-10-31 11:01:25 -0500

    comminting the code

--- target.ino
+++ target.ino
@@ -1,10 +1,9 @@
-//
+
 int threshold = 800;
 void setup()
 {
-  pinMode(A0, INPUT);
-  Serial.begin(9600);
-  
+  pinMode(A0, INPUT); // set the A0 which is the light diode to input; pull down resistor of 2.2k; 
+  Serial.begin(9600);  
 }
 
 void loop()
@@ -14,12 +13,13 @@ void loop()
 }
 
 void CheckIfLaserIsOnSensor(){
-  Serial.println(analogRead(A0));
-  if(analogRead(A0) < threshold){
+  Serial.println(analogRead(A0)); // read the light diode
+  if(analogRead(A0) < threshold){  // if the light diode value is less than 800 than pass
   Serial.println("PASSED Threshold");
-  digitalWrite(13, HIGH);
+  digitalWrite(13, HIGH);  //pass to this and turn on the relay switch to blow up capasitor 
   }else{
-  	digitalWrite(13, LOW);
+  	digitalWrite(13, LOW); // other wise turn off the relay
   	}
 }
+
 
