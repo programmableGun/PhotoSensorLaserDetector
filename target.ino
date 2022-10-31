b00b2e590461a76d48f08f5ec8849f59d8a66c87
Benjamin Santos <67348724+programmableGun@users.noreply.github.com>
2022-10-31 10:46:40 -0500

    cleaned up the code added CheckIFLaserIsOnSensor fution

--- target.ino
+++ target.ino
@@ -9,8 +9,12 @@ void setup()
 
 void loop()
 {
+  delay(10); //small 10ms delay so the arduino doesn't get overloaded;                                                                         
+  CheckIfLaserIsOnSensor();
+}
+
+void CheckIfLaserIsOnSensor(){
   Serial.println(analogRead(A0));
-  delay(10); // Delay a little bit to improve simulation performance
   if(analogRead(A0) < threshold){
   Serial.println("PASSED Threshold");
   digitalWrite(13, HIGH);
@@ -18,3 +22,4 @@ void loop()
   	digitalWrite(13, LOW);
   	}
 }
+
